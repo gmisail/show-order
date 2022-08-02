66e779c4d93c3dfac9dedc24216f0971945a37c1
Graham Misail <gmisail64@gmail.com>
2022-08-02 19:19:11 -0400

    Update README, publish on NPM

--- main.js
+++ main.js
@@ -1,80 +1,95 @@
-import Fuse from 'fuse.js'
-import fs from 'fs';
-import neat from 'neat-csv';
-import readline from 'readline-sync';
+#!/usr/bin/env node
+
+import Fuse from "fuse.js";
+import fs from "fs";
+import neat from "neat-csv";
+import readline from "readline-sync";
 
 const options = {
-    isCaseSensitive: false,
-    
-    keys: [
-      "title"
-    ]
+  isCaseSensitive: false,
+
+  keys: ["title"],
 };
-  
+
 let load_episodes = (directory) => {
-    let all_files = fs.readdirSync(directory);
+  let all_files = fs.readdirSync(directory);
 
-    return all_files.map(file_name => {
-        const extension = file_name.split('.').pop();
+  return all_files.map((file_name) => {
+    const extension = file_name.split(".").pop();
 
-        return {
-            episode_name: undefined,
-            file_name,
-            extension,
-            path: `${directory}/`,
-        };
-    });
-}
+    return {
+      episode_name: undefined,
+      file_name,
+      extension,
+      path: `${directory}/`,
+    };
+  });
+};
 
 let load_series = async (file) => {
-    let file_contents = fs.readFileSync(file);
-    return neat(file_contents);
-}
+  let file_contents = fs.readFileSync(file);
+  return neat(file_contents);
+};
 
 let rename_episode = async (file, episode) => {
-    const season_num = episode.season <= 9 ? `0${episode.season}` : `${episode.season}`;
-    const episode_num = episode.episode <= 9 ? `0${episode.episode}` : `${episode.episode}`;
-    
-    const updated_name = `S${season_num}E${episode_num} - ${episode.title}`;
+  const season_num =
+    episode.season <= 9 ? `0${episode.season}` : `${episode.season}`;
+  const episode_num =
+    episode.episode <= 9 ? `0${episode.episode}` : `${episode.episode}`;
 
-    console.log(`[show-order] rename ${file.file_name} -> ${updated_name}.${file.extension}`)
+  const updated_name = `S${season_num}E${episode_num} - ${episode.title}`;
 
-    fs.renameSync(`${file.path}${file.file_name}`, `${file.path}${updated_name}.${file.extension}`);
-}
+  console.log(
+    `[show-order] rename ${file.file_name} -> ${updated_name}.${file.extension}`
+  );
 
-let process_episode = (episode) => {
-    const episode_name = readline.question(`[show-order] what is the episode name for ${episode.file_name}? `).trim();
-   
-    // pressing return skips this episode
-    if (episode_name.length == 0)
-	return;
+  fs.renameSync(
+    `${file.path}${file.file_name}`,
+    `${file.path}${updated_name}.${file.extension}`
+  );
+};
 
-    const results = finder.search(episode_name);
-    const match = results[0].item;
+let process_episode = (finder, series_table, episode) => {
+  const episode_name = readline
+    .question(
+      `[show-order] what is the episode name for ${episode.file_name}? `
+    )
+    .trim();
 
-    rename_episode(episode, series_table.get(match.title));
-}
+  // pressing return skips this episode
+  if (episode_name.length == 0) {
+    return;
+  }
 
-if (process.argv.length != 4) {
+  const results = finder.search(episode_name);
+  const match = results[0].item;
+
+  rename_episode(episode, series_table.get(match.title));
+};
+
+let run = async () => {
+  if (process.argv.length != 4) {
     console.log("[show-order] expected: <episodes-path> <series csv>");
     process.exit(1);
-}
+  }
 
-const episode_path = process.argv[2];
-const series_csv_path = process.argv[3];
+  const episode_path = process.argv[2];
+  const series_csv_path = process.argv[3];
 
-console.log(`[show-order] loading episodes from ${episode_path}...`);
-const episodes = load_episodes(episode_path);
+  console.log(`[show-order] loading episodes from ${episode_path}...`);
+  const episodes = load_episodes(episode_path);
 
-console.log(`[show-order] loading series from ${series_csv_path}...`);
-const series = await load_series(series_csv_path);
+  console.log(`[show-order] loading series from ${series_csv_path}...`);
+  const series = await load_series(series_csv_path);
 
+  const episode_names = series.map((episode) => episode.title);
+  const finder = new Fuse(series, options);
 
-const episode_names = series.map(episode => episode.title);
-const finder = new Fuse(series, options);
+  let series_table = new Map();
+  series.forEach((episode) => series_table.set(episode.title, episode));
+  episodes.forEach((ep) => process_episode(finder, series_table, ep));
 
-let series_table = new Map();
-series.forEach(episode => series_table.set(episode.title, episode));
-episodes.forEach(process_episode);
+  console.log("[show-order] done.");
+};
 
-console.log("[show-order] done.");
+run().catch((err) => console.error(err));
